aab2fc629b9207c36440f085d91c55e7c6b916e4
Shane Houghton <houghtons@moravian.edu>
2022-08-13 23:51:30 -0400

    initial commit: webserver with one end point and basic api

---
+++ hermes.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup
+
+class Hermes:
+    def __init__(self):
+        self.URL = "https://www.tfrrs.org/"
+    
+
+    def get_roster(self, state, team_name, gender, season):
+        team_html = self.get_team_html(state, team_name, gender, season)
+        roster_rows = self.get_roster_table(team_html)
+        roster = []
+        for athlete_info in roster_rows:
+            name = athlete_info.find_all('td')[0].text.strip()
+            year = athlete_info.find_all('td')[1].text.strip()
+            roster.append({'name':name, 'year':year})
+        return roster
+
+
+    def get_athlete_html(self, name, state, team_name, gender, season):
+        team_html = self.get_team_html(state, team_name, gender, season)
+        roster_rows = self.get_roster_table(team_html)
+        for athlete_info in roster_rows: # does a look up of the athlete by name
+            if name == athlete_info.find_all('td')[0].text.strip().replace(', ','_'):
+                athlete_url = self.URL + athlete_info.find_all('a')[0]['href']
+                break
+        return self.get_soup(athlete_url)
+
+    def get_soup(self, url): # gets html with beautiful soup
+        page = requests.get(url)
+        return BeautifulSoup(page.content, "html.parser")
+
+    def get_year_keys(self, state, team_name, gender): # for getting the key "configure_hnd" so we can get the html page from a certain year
+        soup = self.get_soup(self.URL + f'teams/{state.upper()}_college_{gender.lower()}_{team_name}.html')
+        form_control = soup.find("select", class_="form-control")
+        year_info = form_control.find_all("option")
+        year_info.pop(0) # dictionary comprehension to get the keys and values for the years
+        return {year.text.strip().replace('NCAA','').replace(' ','_').replace('__','_') : str(year).split('"')[1] for year in year_info}
+        
+    def get_team_html(self, state, team_name, gender, season):
+        year_keys = self.get_year_keys(state, team_name, gender)
+        page_url = self.URL + f'teams/{state.upper()}_college_{gender.lower()}_{team_name}.html'
+        page_url += f'?config_hnd={year_keys[season]}'
+        return self.get_soup(page_url)
+
+    def get_roster_table(self, html):
+        html_tables = html.find_all("table", class_="tablesaw")
+        for table in html_tables: # there are multiple tables on some pages especially with track teams
+            if table.find('th').text.strip() == 'NAME': # athlete table has a header with NAME
+                roster_table = table
+        roster_rows = roster_table.find_all("tr")
+        roster_rows.pop(0) # pop the first item in the list because it is just NAME and YEAR
+        return roster_rows
+
---
+++ server.py
@@ -0,0 +1,49 @@
+from flask import Flask, jsonify, request
+from hermes import Hermes
+
+
+app = Flask(__name__)
+hermes = Hermes()
+
+class InvalidAPIUsage(Exception):
+    status_code = 400
+    message = 'Bad request. Check that syntax is correct.'
+    def __init__(self, message=None, status_code=None, payload=None):
+        super().__init__()
+        if message is not None:
+            self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv['message'] = self.message
+        rv['error'] = self.status_code
+        return rv
+
+@app.errorhandler(InvalidAPIUsage)
+def invalid_api_usage(e):
+    return jsonify(e.to_dict()), e.status_code
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+
+@app.get("/roster")
+def get_roster():
+    headers = ['State', 'Team-name', 'Gender', 'Season']
+    header_vals = get_header_vals(headers, request)
+    if None in header_vals:
+        raise InvalidAPIUsage("Headers are incorrect.")
+    state, team_name, gender, season = header_vals
+    try:
+        return jsonify(hermes.get_roster(state, team_name, gender, season))
+    except:
+        raise InvalidAPIUsage()
+
+def get_header_vals(headers, request):
+    vals = [request.headers.get(header) for header in headers]
+    return tuple(vals)
+
